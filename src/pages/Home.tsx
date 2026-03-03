546c4b6305677b1321f7882194ef2a280cbab63f
Jayme <compjme@gmail.com>
2026-03-02 23:41:52 -0500

    Homepage logo added along with introduction

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import logo from "../assets/logo.webp";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>Welcome to</h2>
+      <h1>Immigrant Student Success Office</h1>
+
+      <img
+        src={logo}
+        alt="ISSO logo "
+        style={{ width: "200px", marginTop: "40px" }}
+      />
+    </div>
+  );
 }
 
 export default Home;
