a37bff38ba0a2b77f5088fe64621a8d678b2d668
Jayme <compjme@gmail.com>
2026-03-10 02:18:09 -0400

    Added logo to nav bar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import ISSOLOGO from "../../assets/ISSOLOGO.png";
 
 const links = [
   { path: "/", label: "Home" },
@@ -14,10 +15,17 @@ const linkStyle = ({ isActive }: { isActive: boolean }) => ({
   fontWeight: isActive ? "700" : "400",
   textDecoration: isActive ? "underline" : "none",
 });
+
 //cleans up the navigation bar so that it adds styling when active (navlink>link)
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar-left">
+        <NavLink to="/" className="navbar-logo-link">
+          <img src={ISSOLOGO} alt="Company Logo" className="navbar-logo" />
+        </NavLink>
+      </div>
+
       {links.map((link) => (
         <NavLink key={link.path} to={link.path} style={linkStyle}>
           {link.label}
