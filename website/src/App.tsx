282c5adabecf133366443cd90c884096fb3e7460
MAMAT15 <135150700+MAMAT15@users.noreply.github.com>
2026-03-12 23:45:31 -0400

    Add files via upload

---
+++ website/src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import Scheduling from "./pages/Scheduling";
+import Community from "./pages/Community";
+import Resources from "./pages/Resources";
+import FAQChatbot from "./pages/FAQChatbot";
+import Notfound from "./pages/Notfound";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/scheduling" element={<Scheduling />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/faq" element={<FAQChatbot />} />
+        <Route path="*" element={<Notfound />} />
+      </Route>
+    </Routes>
+
+  );
+}
+
+export default App;
