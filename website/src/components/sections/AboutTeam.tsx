9f156505f61f9e3346b79f5273debb8a756ae545
Jayme <compjme@gmail.com>
2026-03-25 18:19:14 -0400

    removed dupe files

---
+++ website/src/components/sections/AboutTeam.tsx
@@ -0,0 +1,13 @@
+import "./AboutTeam.css";
+
+const AboutTeam = () => {
+  return (
+    <>
+      <section className="about-team">
+        <h2>About Our Team</h2>
+      </section>
+    </>
+  );
+};
+
+export default AboutTeam;
--- website/src/pages/Home.tsx
+++ website/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/sections/Hero";
+import AboutTeam from "../components/sections/AboutTeam";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <AboutTeam />
     </div>
   );
 }
