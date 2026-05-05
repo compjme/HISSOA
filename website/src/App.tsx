6e3d7098c0a153a09afc1de57ae565ab10eed1f5
Ax3lMejia <axelmejia2851@gmail.com>
2026-05-05 04:56:25 -0400

    Add supabase auth setup(reminder to remove dev bypass in the protectedRoute.tsx file)

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -9,6 +9,7 @@ import Resources from "./pages/Resources";
 import FAQChatbot from "./pages/FAQChatbot";
 import Notfound from "./pages/Notfound";
 import Layout from "./components/layout/Layout";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
@@ -16,11 +17,11 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/scheduling" element={<Scheduling />} />
-        <Route path="/community" element={<Community />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/faq" element={<FAQChatbot />} />
         <Route path="*" element={<Notfound />} />
+        <Route path="/scheduling" element={<ProtectedRoute> <Scheduling /> </ProtectedRoute> }/>
+        <Route path="/community" element={ <ProtectedRoute> <Community /> </ProtectedRoute> }/>
       </Route>
     </Routes>
 
