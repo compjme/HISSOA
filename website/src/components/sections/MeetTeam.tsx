e6f67276ce4797c93c49759b263cc50abe930ef8
Jayme <compjme@gmail.com>
2026-05-06 00:51:44 -0400

    added the meet the team information to the section

--- website/src/components/sections/MeetTeam.tsx
+++ website/src/components/sections/MeetTeam.tsx
@@ -1,10 +1,56 @@
 import "./MeetTeam.css";
+import leslie from "../../assets/leslie.jpg";
+import jesus from "../../assets/jesus.jpg";
 
 const MeetTeam = () => {
+  const teamMembers = [
+    // array for the team members
+    {
+      name: "Jesus Perez",
+      role: "Director, Immigrant Student Success Office",
+      email: "jperez@brooklyn.cuny.edu",
+      image: jesus,
+    },
+
+    {
+      name: "Leslie Ramirez Carbajal",
+      role: "Program Coordinator, Immigrant Student Success Office",
+      email: "l.ramirezcarbajal@brooklyn.cuny.edu",
+      image: leslie,
+    },
+  ];
+
   return (
-    <section id="Team" className="meet-team">
+    <section id="Team" className="team-section">
       <div className="team-card">
-        <div className="team-section"></div>
+        <div className="team-header">
+          <h2>Meet the Team</h2>
+          <p>
+            The Immigrant Student Success office, team of committed staff whose
+            focus is to support undocumented and immigrant students as they
+            navigate their academic and personal journeys. At ISSO, we proudly
+            serve students by providing guidance, resources, and a community of
+            care to ensure every individual feels valued, seen, and empowered.
+          </p>
+        </div>
+
+        <div className="team-grid">
+          {teamMembers.map((member) => (
+            <div className="team-member-card" key={member.email}>
+              <img
+                src={member.image}
+                alt={`${member.name} profile`}
+                className="team-image"
+              />
+
+              <h3>{member.name}</h3>
+              <p className="team-role">{member.role}</p>
+              <a href={`mailto:${member.email}`} className="team-email">
+                {member.email}
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
