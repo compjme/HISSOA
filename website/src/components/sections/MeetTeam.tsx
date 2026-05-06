a84712c23b5e334e7ba1289a99f668c5665f6a53
Jayme <compjme@gmail.com>
2026-05-06 01:12:15 -0400

    adjustments for the meet the team section (formatting)

--- website/src/components/sections/MeetTeam.tsx
+++ website/src/components/sections/MeetTeam.tsx
@@ -7,21 +7,24 @@ const MeetTeam = () => {
     // array for the team members
     {
       name: "Jesus Perez",
-      role: "Director, Immigrant Student Success Office",
+      role: "Director of ISSO",
       email: "jperez@brooklyn.cuny.edu",
       image: jesus,
+      description:
+        "The founding director of the Immigrant Student Success Office (ISSO) at Brooklyn College, where he has led since 2019 with a mission to empower immigrant and first-generation students to achieve their academic dreams. Under his leadership, ISSO has become a critical resource, providing essential support that enables students to overcome barriers and thrive.",
     },
-
     {
       name: "Leslie Ramirez Carbajal",
-      role: "Program Coordinator, Immigrant Student Success Office",
+      role: "Program Coordinator of ISSO",
       email: "l.ramirezcarbajal@brooklyn.cuny.edu",
       image: leslie,
+      description:
+        "dedicated to empowering undocumented and immigrant students, currently serving as a key advocate at Brooklyn College's Immigrant Student Success Office as the Program Coordinator. She previously played a pivotal role at John Jay College’s Immigrant Student Success Center, the first of its kind within the CUNY system, where she helped students navigate complex challenges and access vital resources.",
     },
   ];
 
   return (
-    <section id="Team" className="team-section">
+    <section id="team" className="team-section">
       <div className="team-card">
         <div className="team-header">
           <h2>Meet the Team</h2>
@@ -42,7 +45,6 @@ const MeetTeam = () => {
                 alt={`${member.name} profile`}
                 className="team-image"
               />
-
               <h3>{member.name}</h3>
               <p className="team-role">{member.role}</p>
               <a href={`mailto:${member.email}`} className="team-email">
